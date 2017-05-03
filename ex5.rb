9592c680a13bdeb451721ac04a9d45ba36e4beaf
Christophe Boutter <christophe.boutter@saarstahl.com>
2017-05-03 17:09:44 +0200

    Exercise 5, without study

---
+++ ex5.rb
@@ -0,0 +1,13 @@
+my_name = 'Chris Boutter'
+my_age = 35 # not a lie in 2017
+my_height = 175
+my_weight = 80 # this might be a lie :D
+my_eyes = 'Gray'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_weight} kg heavy."
+puts "Actually that's not too heavy."
+
+puts "If I add #{my_age}, #{my_height} and #{my_weight} I get #{my_age + my_height + my_weight}."
